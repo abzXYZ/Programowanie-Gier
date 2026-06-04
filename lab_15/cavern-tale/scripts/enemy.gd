712fc93b68a1c8c5f36c98968c23edf75feffa05
abzXYZ <80230031+abzXYZ@users.noreply.github.com>
2026-06-04 02:46:52 +0200

    Poprawa drobnych usterek

--- lab_15/cavern-tale/scripts/enemy.gd
+++ lab_15/cavern-tale/scripts/enemy.gd
@@ -1,7 +1,7 @@
 extends Area2D
 class_name Enemy
 
-var particle : PackedScene	# Particle instance (impact on hit/smoke on death)
+@export var particle : PackedScene	# Particle instance (impact on hit/smoke on death)
 var max_hp : int
 var xp_awarded : float		# XP added to active weapon's lv after kill
 var touch_damage : int		# Damage dealt to the player upon touch
